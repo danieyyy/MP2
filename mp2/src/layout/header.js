f6795a181d1dae06da84ae4fcae9e4e9fcaaaebd
danieyyy <daniellaannagrenmo@gmail.com>
2023-08-10 18:08:55 +0800

    added layouts

---
+++ mp2/src/layout/header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return(
+        <header>
+            <nav>
+                <div className="header__container">
+
+                </div>
+                
+            </nav>
+        </header>
+    )
+}
+export default Header;
