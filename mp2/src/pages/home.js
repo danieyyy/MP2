26958935c5e8103a5d8b784951fc8cfbb483aa26
danieyyy <daniellaannagrenmo@gmail.com>
2023-08-25 20:59:16 +0800

    edited routes for homepage

--- mp2/src/pages/home.js
+++ mp2/src/pages/home.js
@@ -3,6 +3,7 @@ import Modules from '../assets/modules.png'
 import Milestones from '../assets/milestones.png'
 import Database from '../assets/database.png'
 import Feedback from '../assets/feedback.png'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
@@ -22,11 +23,11 @@ const Home = () => {
                         This online onboarding will help you start your journey with ABC Tech Inc. Take a deep breath and relax... because we are about to take you to a joyful ride as you learn more about our company, make new connections, and feel the excitement as we welcome you in our team!
                     </p>
                     <div class="content__iconContainer">
-                        <div class="icon__dashboard iconItem"><a href="dashboard.html" class="iconImg"><img src={Dashboard} alt="" id="icon__dashboard" class="icon__unit"/></a></div>
-                        <div class="icon__modules iconItem"><a href="modules.html" class="iconImg"><img src={Modules} alt="" id="icon__modules" class="icon__unit"/></a></div>
-                        <div class="icon__milestones iconItem"><a href="milestones.html" class="iconImg"><img src={Milestones} alt="" id="icon__milestones" class="icon__unit"/></a></div>
-                        <div class="icon__database iconItem"><a href="database.html" class="iconImg"><img src={Database} alt="" id="icon__database" class="icon__unit"/></a></div>
-                        <div class="icon__feedback iconItem"><a href="feedback.html" class="iconImg"><img src={Feedback} alt="" id="icon__feedback" class="icon__unit"/></a></div>
+                        <div class="icon__dashboard iconItem"><Link to="/dashboard" class="iconImg"><img src={Dashboard} alt="" id="icon__dashboard" class="icon__unit"/></Link></div>
+                        <div class="icon__modules iconItem"><Link to="/modules" class="iconImg"><img src={Modules} alt="" id="icon__modules" class="icon__unit"/></Link></div>
+                        <div class="icon__milestones iconItem"><Link to="/milestones" class="iconImg"><img src={Milestones} alt="" id="icon__milestones" class="icon__unit"/></Link></div>
+                        <div class="icon__database iconItem"><Link to="/database" class="iconImg"><img src={Database} alt="" id="icon__database" class="icon__unit"/></Link></div>
+                        <div class="icon__feedback iconItem"><Link to="/feedback" class="iconImg"><img src={Feedback} alt="" id="icon__feedback" class="icon__unit"/></Link></div>
                     </div>
                     {/* <Card style={{ width: '18rem' }}>
                         <Card.Img variant="top" src="holder.js/100px180" />
